51803f0a356e594bf64a504a24ec650dc4118706
Sandeepgowda15 <155448457+Sandeepgowda15@users.noreply.github.com>
2026-08-24 19:26:41 +0530

    Refactor ECR login and pull messages in start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-echo "Logging in to Amazon ECR..."
+echo "Logging in to ECR..."
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 089783390772.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | \
+docker login --username AWS --password-stdin \
+089783390772.dkr.ecr.us-east-1.amazonaws.com
 
-echo "Pulling latest React image..."
+echo "Pulling React image..."
 
 docker pull 089783390772.dkr.ecr.us-east-1.amazonaws.com/react-project:latest
 
 echo "Starting React container..."
+
+docker stop react-project 2>/dev/null || true
+docker rm react-project 2>/dev/null || true
 
 docker run -d \
   --name react-project \
