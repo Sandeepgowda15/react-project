2d6fadd413b04141b6c2903984483ad1682a5f90
Sandeepgowda15 <155448457+Sandeepgowda15@users.noreply.github.com>
2026-08-24 11:05:47 +0530

    Add start script for React application deployment

---
+++ scripts/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Logging in to Amazon ECR..."
+
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 089783390772.dkr.ecr.us-east-1.amazonaws.com
+
+echo "Pulling latest React image..."
+
+docker pull 089783390772.dkr.ecr.us-east-1.amazonaws.com/react-project:latest
+
+echo "Starting React container..."
+
+docker run -d \
+  --name react-project \
+  -p 80:80 \
+  089783390772.dkr.ecr.us-east-1.amazonaws.com/react-project:latest
+
+echo "React application started successfully."
